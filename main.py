c92470eb6f1f7bf2e50c1c33a45c5763a4553f25
haael <haael@interia.pl>
2020-05-01 19:05:49 +0100

    Conflict resolution.

--- main.py
+++ main.py
@@ -1,12 +1,9 @@
 #!/usr/bin/python3
 
 import gi
-
 gi.require_version('Gtk', '3.0')
-gi.require_version('WebKit2', '4.0')
-
-from gi.repository import Gtk, GLib, Gdk, WebKit2
-
+from gi.repository import Gtk, GLib, Gdk, Gst
+import os, time
 if __name__ == '__main__':
 	import sys, signal
 	
@@ -29,24 +26,19 @@ if __name__ == '__main__':
 			self.builder = Gtk.Builder()
 			self.builder.add_from_file('videoplayer2.glade')
 			self.builder.connect_signals(self)
+			Gst.init(None)
+			self.player = Gst.ElementFactory.make("playbin", "player")
+			self.movie_window=self.builder.get_object("drawingarea1")
+			bus = self.player.get_bus()
+			bus.add_signal_watch()
+			bus.connect("message", self.on_message)
+			bus.connect("sync-message::element",self.on_sync_message)
 			
-			self.webview = WebKit2.WebView()
-			self.notebook1.append_page(self.webview)
-			self.webview.show()
-			self.notebook1.set_current_page(1)
-			self.webview.load_uri('https://raw.githubusercontent.com/haael/white-box-fapkc/master/README.md')
-			
-			#self.drawingarea1.connect('draw', self.draw)
-		
 		def __getattr__(self, attr):
 			widget = self.builder.get_object(attr)
 			if widget == None:
 				raise AttributeError("Widget not found: " + attr)
 			return widget
-		
-		def draw(self, widget, ctx):
-			ctx.set_source_rgb(0.7, 0.7, 1)
-			ctx.paint()
 		
 		@idle_add
 		def test(self, *args):
@@ -54,13 +46,33 @@ if __name__ == '__main__':
 		
 		@idle_add
 		def quit(self, *args):
-			mainloop.quit()
-		
+						print("quit")
+						mainloop.quit()
 		@idle_add
-		def mouse(self, widget, event):
-			self.progressbar1.set_fraction(event.x / self.progressbar1.get_allocated_width())
-			self.progresstext1.set_text(str(int(event.x)))
-	
+		def open_url(self, *args):
+			print("current working directory", os.getcwd())
+			filepath = os.getcwd()+"/"+self.entry1.get_text().strip()
+			if os.path.isfile(filepath):
+				filepath = os.path.realpath(filepath)
+				self.player.set_property("uri", "file://" + filepath)
+				self.player.set_state(Gst.State.PLAYING)
+			else:
+				self.player.set_state(Gst.State.NULL)
+		def on_message(self, bus, message):
+			t = message.type
+			if t == Gst.MessageType.EOS:
+				self.player.set_state(Gst.State.NULL)
+			elif t == Gst.MessageType.ERROR:
+				self.player.set_state(Gst.State.NULL)
+				err, debug = message.parse_error()
+				print("Error: {}".format(err, debug))
+		def on_sync_message(self, bus, message):
+			if message.get_structure().get_name() == 'prepare-window-handle':
+				imagesink = message.src
+				imagesink.set_property("force-aspect-ratio", True)
+				imagesink.set_window_handle(self.movie_window.get_property('window').get_xid())
+
+
 	interface = Interface()
 	interface.window1.show_all()
 	
