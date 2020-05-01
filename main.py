f121441b8468763bfb59ccf2e71d7882fad154e6
JanKesek <4chewbaca@gmail.com>
2020-05-01 20:02:03 +0200

    Playing local file in external window

--- main.py
+++ main.py
@@ -2,8 +2,8 @@
 
 import gi
 gi.require_version('Gtk', '3.0')
-from gi.repository import Gtk, GLib, Gdk
-
+from gi.repository import Gtk, GLib, Gdk, Gst
+import os, time
 if __name__ == '__main__':
 	import sys, signal
 	
@@ -24,9 +24,16 @@ if __name__ == '__main__':
 	class Interface:
 		def __init__(self):
 			self.builder = Gtk.Builder()
-			self.builder.add_from_file('interface.glade')
+			self.builder.add_from_file('videoplayer2.glade')
 			self.builder.connect_signals(self)
-		
+			Gst.init(None)
+			self.player = Gst.ElementFactory.make("playbin", "player")
+			self.movie_window=self.builder.get_object("drawingarea1")
+			bus = self.player.get_bus()
+			bus.add_signal_watch()
+			bus.connect("message", self.on_message)
+			bus.connect("sync-message::element",self.on_sync_message)
+			
 		def __getattr__(self, attr):
 			widget = self.builder.get_object(attr)
 			if widget == None:
@@ -39,8 +46,33 @@ if __name__ == '__main__':
 		
 		@idle_add
 		def quit(self, *args):
-			mainloop.quit()
-	
+						print("quit")
+						mainloop.quit()
+		@idle_add
+		def open_url(self, *args):
+			print("current working directory", os.getcwd())
+			filepath = os.getcwd()+"/"+self.entry1.get_text().strip()
+			if os.path.isfile(filepath):
+				filepath = os.path.realpath(filepath)
+				self.player.set_property("uri", "file://" + filepath)
+				self.player.set_state(Gst.State.PLAYING)
+			else:
+				self.player.set_state(Gst.State.NULL)
+		def on_message(self, bus, message):
+			t = message.type
+			if t == Gst.MessageType.EOS:
+				self.player.set_state(Gst.State.NULL)
+			elif t == Gst.MessageType.ERROR:
+				self.player.set_state(Gst.State.NULL)
+				err, debug = message.parse_error()
+				print("Error: {}".format(err, debug))
+		def on_sync_message(self, bus, message):
+			if message.get_structure().get_name() == 'prepare-window-handle':
+				imagesink = message.src
+				imagesink.set_property("force-aspect-ratio", True)
+				imagesink.set_window_handle(self.movie_window.get_property('window').get_xid())
+
+
 	interface = Interface()
 	interface.window1.show_all()
 	
